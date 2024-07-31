103949fccf3342d2047697a9774398858ead221d
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-07-31 18:37:42 +0200

    Mix two textures

--- LearnOpenGL/LearnOpenGL/Main.cpp
+++ LearnOpenGL/LearnOpenGL/Main.cpp
@@ -106,18 +106,18 @@ int main() {
 
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);   // uncomment this call to draw in wireframe polygons
 
-    // Load and create a texture 
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
-    
+    // Load and create textures
+    unsigned int texture1, texture2;
+
+    glGenTextures(1, &texture1);    // texture1
+    glBindTexture(GL_TEXTURE_2D, texture1);                                         // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	                // set the texture wrapping parameters, (GL_REPEAT is default wrapping method)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // set texture filtering parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
-    int width, height, nrChannels; // load image, create texture and generate mipmaps,
+    stbi_set_flip_vertically_on_load(true);     // tell stb_image.h to flip loaded texture's on the y-axis.
+    int width, height, nrChannels;              // load image, create texture and generate mipmaps
     unsigned char* data = stbi_load("images/container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
@@ -130,7 +130,31 @@ int main() {
     }
     stbi_image_free(data);
 
+    glGenTextures(1, &texture2);    // texture2
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    data = stbi_load("images/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 
+    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once), and don't forget to activate/use the shader before setting uniforms!
+    ourShader.use();
+    ourShader.setInt("texture1", 0);    // 0 for GL_TEXTURE0
+    ourShader.setInt("texture2", 1);    // 1 for GL_TEXTURE1
+    
     // Run the program
     while (!glfwWindowShouldClose(window))
     {
@@ -139,9 +163,12 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
         ourShader.use();
-        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO); 
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
