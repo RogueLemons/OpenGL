6df7a220259ac0759b5c9f0b75d34aa0e4d9bd1c
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-08-23 10:28:54 +0200

    Add test code to draw triangle (something not working)

--- SimpleGraphics/TestProject/Main.cpp
+++ SimpleGraphics/TestProject/Main.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
+#include <vector>
+
+// Charis
 #include "Charis/Initialize.h"
 #include "Charis/Utility.h"
+#include "Charis/Model.h"
 #include "Charis/Shader.h"
+
+struct Vertex {
+    float x{};
+    float y{};
+    float z{};
+};
 
 int main()
 {
     Charis::Initialize();
 
+    const std::vector<float> vertices = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f
+    };
+    const auto triangle = Charis::Model(vertices, {3});
+
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "layout (location = 0) out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0";
+    const auto shader = Charis::Shader(vertexShaderSource, fragmentShaderSource, Charis::Shader::InCode);
+
+
     while (Charis::WindowIsOpen()) {
         Charis::InitializeLoop();
         if (Charis::Input::KeyState(Charis::Input::Escape, Charis::Input::Pressed))
             Charis::Utility::CloseWindow();
+
+        shader.Draw(triangle);
         
         if (Charis::Input::KeyState(Charis::Input::W, Charis::Input::Pressed)) {
             std::cout << "W" << std::endl;
