c2bc9e3a05bd5f59a59c1670629ee6ea35d7f4f4
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-08-25 17:51:19 +0200

    asd

--- SimpleGraphics/Charis/Texture.cpp
+++ SimpleGraphics/Charis/Texture.cpp
@@ -13,14 +13,19 @@ namespace Charis {
         glGenTextures(1, &m_ID);
         glBindTexture(GL_TEXTURE_2D, m_ID);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	                // set the texture wrapping parameters, (GL_REPEAT is default wrapping method)
+        // set the texture wrapping parameters, (GL_REPEAT is default wrapping method)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // set texture filtering parameters
+        // set texture filtering parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-        stbi_set_flip_vertically_on_load(true);     // tell stb_image.h to flip loaded texture's on the y-axis, consider moving to Charis initialization
-        int width, height, nrChannels;              // load image, create texture and generate mipmaps
-        unsigned char* data = stbi_load("images/container.jpg", &width, &height, &nrChannels, 0);
+        // tell stb_image.h to flip loaded texture's on the y-axis, consider moving to Charis initialization
+        stbi_set_flip_vertically_on_load(true);
+
+        // load image, create texture and generate mipmaps
+        int width, height, nrChannels;
+        unsigned char* data = stbi_load(pathToImage.data(), &width, &height, &nrChannels, 0);
         Helper::RuntimeAssert(data, "Failed to load texture: " + pathToImage);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
