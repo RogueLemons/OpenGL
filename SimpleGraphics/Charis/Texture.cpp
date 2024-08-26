e50bf3698d050c4c2a952aeba96054d7088a3dc3
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-08-26 16:42:37 +0200

    Remove unnecessary bindingCodes array

--- SimpleGraphics/Charis/Texture.cpp
+++ SimpleGraphics/Charis/Texture.cpp
@@ -46,41 +46,8 @@ namespace Charis {
     void Texture::BindTo(unsigned int binding)
     {
         Helper::RuntimeAssert(31 >= binding && binding >= 0, "Texture global state binding index must be in the range [0, 31].");
-        constexpr std::array<int, 32> bindingCodes = {
-            0x84C0,
-            0x84C1,
-            0x84C2,
-            0x84C3,
-            0x84C4,
-            0x84C5,
-            0x84C6,
-            0x84C7,
-            0x84C8,
-            0x84C9,
-            0x84CA,
-            0x84CB,
-            0x84CC,
-            0x84CD,
-            0x84CE,
-            0x84CF,
-            0x84D0,
-            0x84D1,
-            0x84D2,
-            0x84D3,
-            0x84D4,
-            0x84D5,
-            0x84D6,
-            0x84D7,
-            0x84D8,
-            0x84D9,
-            0x84DA,
-            0x84DB,
-            0x84DC,
-            0x84DD,
-            0x84DE,
-            0x84DF
-        };
-        glActiveTexture(bindingCodes.at(binding));
+        int bindingCode = binding + 0x84C0;
+        glActiveTexture(bindingCode);
         glBindTexture(GL_TEXTURE_2D, m_ID);
     }
 
