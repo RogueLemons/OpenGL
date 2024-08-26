548f96973b26466946517c1a960c99e3bbdd9583
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-08-26 17:33:09 +0200

    Combine SimpleShader and Shader into one class

--- SimpleGraphics/CharisAPI/include/Charis/Shader.h
+++ SimpleGraphics/CharisAPI/include/Charis/Shader.h
@@ -1,34 +1,55 @@
 #pragma once
-#include "SimpleShader.h"
+#include "Model.h"
+#include <string>
+#include <vector>
 
 // Libraries
 #include <glm/glm.hpp>
 
 namespace Charis {
 
-    /// <summary>An easy to use shader with GLM support. Contains methods to draw models and set shader uniform variables.</summary>
-	class Shader : public SimpleShader
+	/// <summary>An easy to use shader with GLM support. Contains methods to draw models and set shader uniform variables.</summary>
+	class Shader
 	{
 	public:
-        /// <summary>
-        /// Constructor for a simple shader class with GLM support. 
-        /// </summary>
-        /// <param name="vertexShader">Contains either the path to a file with vertex shader source code or the source code directly.</param>
-        /// <param name="fragmentShader">Contains either the path to a file with fragment shader source code or the source code directly.</param>
-        /// <param name="inputType">Decides if arguments are treated as file paths or source code. Default as file path.</param>
-        Shader(const std::string& vertexShader, const std::string& fragmentShader, InputType inputType = Filepath) : SimpleShader(vertexShader, fragmentShader, inputType) {}
-		
-        void SetVec2(const std::string& name, const glm::vec2& value) const;
-        void SetVec2(const std::string& name, float x, float y) const;
-        void SetVec3(const std::string& name, const glm::vec3& value) const;
-        void SetVec3(const std::string& name, float x, float y, float z) const;
-        void SetVec4(const std::string& name, const glm::vec4& value) const;
-        void SetVec4(const std::string& name, float x, float y, float z, float w) const;
-        void SetMat2(const std::string& name, const glm::mat2& mat) const;
-        void SetMat3(const std::string& name, const glm::mat3& mat) const;
-        void SetMat4(const std::string& name, const glm::mat4& mat) const;
+		enum InputType {
+			// Provides paths to files with shader code to constructor.
+			Filepath,
+			// Shader source code is provided directly as a string to constructor.
+			InCode
+		};
+		/// <summary>
+		/// Constructor for a shader class.
+		/// </summary>
+		/// <param name="vertexShader">Contains either the path to a file with vertex shader source code or the source code directly.</param>
+		/// <param name="fragmentShader">Contains either the path to a file with fragment shader source code or the source code directly.</param>
+		/// <param name="inputType">Decides if arguments are treated as file paths or source code. Default as file path.</param>
+		Shader(const std::string& vertexShader, const std::string& fragmentShader, InputType inputType = Filepath);
+		~Shader();
+
+		// Uses this shader to draw a model.
+		void Draw(const Model& model) const;
+		// Uses this shader to draw a list of models.
+		void Draw(const std::vector<Model>& models) const;
+
+		void SetBool(const std::string& name, bool value) const;
+		void SetInt(const std::string& name, int value) const;
+		void SetFloat(const std::string& name, float value) const;
+		void SetTexture(const std::string& name, unsigned int binding) const;
+
+		void SetVec2(const std::string& name, const glm::vec2& value) const;
+		void SetVec2(const std::string& name, float x, float y) const;
+		void SetVec3(const std::string& name, const glm::vec3& value) const;
+		void SetVec3(const std::string& name, float x, float y, float z) const;
+		void SetVec4(const std::string& name, const glm::vec4& value) const;
+		void SetVec4(const std::string& name, float x, float y, float z, float w) const;
+		void SetMat2(const std::string& name, const glm::mat2& mat) const;
+		void SetMat3(const std::string& name, const glm::mat3& mat) const;
+		void SetMat4(const std::string& name, const glm::mat4& mat) const;
+
+
+	protected:
+		unsigned int m_ID;
 	};
 
 }
-
-
