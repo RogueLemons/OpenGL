bf6bb57e4178ff129b09ef5d2fa2548576833a87
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-08-27 12:33:51 +0200

    Make all images load as RGBA in Texture class

--- SimpleGraphics/Charis/Texture.cpp
+++ SimpleGraphics/Charis/Texture.cpp
@@ -25,7 +25,8 @@ namespace Charis {
 
         // load image, create texture and generate mipmaps
         int width, height, nrChannels;
-        unsigned char* data = stbi_load(pathToImage.data(), &width, &height, &nrChannels, 0);
+        const int desiredNrChannels = 4;
+        unsigned char* data = stbi_load(pathToImage.data(), &width, &height, &nrChannels, desiredNrChannels);
         Helper::RuntimeAssert(data, "Failed to load texture: " + pathToImage);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
