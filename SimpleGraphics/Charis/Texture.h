0983d44828e38acd8953256c860825f892960e1e
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-08-27 12:47:15 +0200

    Remove unnecessary Texture constructor that only causes confusion

--- SimpleGraphics/Charis/Texture.h
+++ SimpleGraphics/Charis/Texture.h
@@ -13,10 +13,6 @@ namespace Charis {
 		/// <summary>Constructor for a texture.</summary>
 		/// <param name="pathToImage">File path to image to load into texture.</param>
 		Texture(const std::string& pathToImage);
-		/// <summary>Constructor for a texture. By adding a texture binding to the constructor the image is loaded as a texture and immedeately bound to the given global state.</summary>
-		/// <param name="pathToImage">File path to image to load into texture. The global state binding index that should be used to access this texture.</param>
-		/// <param name="binding">Value must be in [0, 31] range.</param>
-		Texture(const std::string& pathToImage, unsigned int binding);
 		~Texture();
 
 		/// <summary>
--- SimpleGraphics/CharisAPI/include/Charis/Texture.h
+++ SimpleGraphics/CharisAPI/include/Charis/Texture.h
@@ -13,17 +13,13 @@ namespace Charis {
 		/// <summary>Constructor for a texture.</summary>
 		/// <param name="pathToImage">File path to image to load into texture.</param>
 		Texture(const std::string& pathToImage);
-		/// <summary>Constructor for a texture. By adding a texture binding to the constructor the image is loaded as a texture and immedeately bound to the given global state.</summary>
-		/// <param name="pathToImage">File path to image to load into texture. The global state binding index that should be used to access this texture.</param>
-		/// <param name="binding">Value must be in [0, 31] range.</param>
-		Texture(const std::string& pathToImage, unsigned int binding);
 		~Texture();
 
 		/// <summary>
 		/// This function binds the texture to one of the 32 global texture states. Shaders access textures from the global states so this is a requirement for shaders.
 		/// </summary>
 		/// <param name="binding">Value must be in [0, 31] range. The global state binding index that should be used to access this texture.</param>
-		void BindTo(unsigned int binding);
+		void BindTo(unsigned int binding) const;
 	private:
 		unsigned int m_ID;
 	};
