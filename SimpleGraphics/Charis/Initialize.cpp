82eeb91d8295e9feeb55eab024ebcc0e6b0e348e
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-09-13 10:49:39 +0200

    Update to latest OpenGL context for glfw

--- SimpleGraphics/Charis/Initialize.cpp
+++ SimpleGraphics/Charis/Initialize.cpp
@@ -32,8 +32,8 @@ namespace Charis {
 	{
         // glfw: initialize and configure
 		Helper::RuntimeAssert(glfwInit() == GLFW_TRUE, "Failed to initialize GLFW.");
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
